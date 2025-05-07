a4a8186635bb6c5cd10c21498a8cd818114f5aa8
Robn8 <rob.natale.8@gmail.com>
2025-05-07 17:56:51 -0500

    Mission box component update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
     return (
         <>
             <div class="flex flex-col items-center">
-                <img class="mt-10 mb-4" src="pic1.png" alt="Yoke" />
-                <p class="font-">A Promise 2 Papa</p>
+                <img class="mt-10 mb-4 w-125" src="pic1.png" alt="Yoke" />
+                <p class="font-">Promise 2 Papa</p>
             </div>
         </>
     )
--- src/Components/MissionBox.jsx
+++ src/Components/MissionBox.jsx
@@ -1,8 +1,11 @@
 function MissionBox() {
     return (
         <div>
-            <div>
-                
+            <div class="flex flex-col mt-5 items-center">
+                <div class="flex flex-row w-200 h-100">
+                <img class="mt-10 mb-4 w-125" src="pic2.png" alt="Yoke" />
+                <img class="mt-10 mb-4 w-125" src="pic3.png" alt="Yoke" />
+                </div>
             </div>
         </div>
     )
