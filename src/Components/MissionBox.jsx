83494fd3dc5685e30c9c7c587d50fe2004b5884c
Robn8 <rob.natale.8@gmail.com>
2025-05-17 15:05:52 -0500

    MissionBox component update

--- src/Components/MissionBox.jsx
+++ src/Components/MissionBox.jsx
@@ -1,11 +1,18 @@
 function MissionBox() {
     return (
-        <div className="flex justify-center mt-10 ">
-            <div className="flex flex-row w-[900px] h-80 justify-center items-center bg-gray-100 rounded-lg shadow-md">
-
+        <div className="flex justify-center mt-10">
+            <div className="flex flex-row w-[900px] h-80 bg-gray-100 rounded-lg shadow-md overflow-hidden">
+                <div className="flex-2">
+                    <img src="pic4.jpg" alt="Image 1" className="w-full h-full object-cover" />
+                </div>
+                <div className="flex-1">
+                    <img src="pic3.png" alt="Image 2" className="w-80 h-full " />
+                </div>
             </div>
         </div>
     );
 }
+
+
 
 export default MissionBox;
