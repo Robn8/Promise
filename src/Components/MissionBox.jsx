f0b1b90b412eaccd994af9c9f86cd1c6b27f9add
Robn8 <rob.natale.8@gmail.com>
2025-05-30 16:04:23 -0500

    MissionBox bg update.

--- src/Components/MissionBox.jsx
+++ src/Components/MissionBox.jsx
@@ -1,18 +1,22 @@
 function MissionBox() {
     return (
-        <div className="flex justify-center mt-10">
-            <div className="flex flex-row w-[900px] h-[400px] bg-gray-100 rounded-lg shadow-md overflow-hidden">
+        <div className="relative min-h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center pt-10"
+            style={{ backgroundImage: `url('/pic2.png')` }}
+        >
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black/40 backdrop-blur-sm"></div>
+
+            {/* Content */}
+            <div className="relative z-10 mt-5 flex flex-row w-[900px] h-[400px] rounded-lg shadow-md overflow-hidden">
                 <div className="flex-2">
                     <img src="pic4.jpg" alt="Image 1" className="w-full h-full object-cover" />
                 </div>
                 <div className="flex-1">
-                    <img src="pic3.png" alt="Image 2" className="w-80 h-full " />
+                    <img src="pic3.png" alt="Image 2" className="w-80 h-full" />
                 </div>
             </div>
         </div>
     );
 }
-
-
 
 export default MissionBox;
