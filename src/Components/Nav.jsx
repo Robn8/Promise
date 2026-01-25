5b3d8cbaaaa0949ca303da90b7678d57b53582c3
Robn8 <rob.natale.8@gmail.com>
2026-01-25 14:50:34 -0600

    Nav logo style circle edit

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -46,13 +46,17 @@ function Nav() {
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between md:justify-between">
 
         {/* LEFT: Logo */}
-        <NavLink to="/" className="flex items-center">
-          <img
-            src="/hand1.jpg"
-            alt="Promise2Papa Logo"
-            className="w-20 h-20 object-contain"
-          />
-        </NavLink>
+<NavLink to="/" className="flex items-center mr-4 md:mr-6 shrink-0">
+  <div className="p-1 rounded-full bg-blue-600/10 ring-2 ring-blue-500 shadow-sm transition hover:scale-105">
+    <img
+      src="/hand1.jpg"
+      alt="Promise2Papa Logo"
+      className="w-14 h-14 md:w-16 md:h-16 rounded-full object-cover bg-white"
+    />
+  </div>
+</NavLink>
+
+
 
         {/* CENTER: Title */}
         <NavLink
